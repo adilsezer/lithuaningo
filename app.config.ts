69e3cb38158b67bb8d8ab995be2c94fbda895257
sezerad <sezeradil@gmail.com>
2024-07-02 01:32:07 +0100

    updated version

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ...config,
     name: "Lithuaningo",
     slug: "lithuaningo",
-    version: "1.3.0",
+    version: "1.4.0",
     orientation: "portrait",
     icon: "./assets/icons/ios/icon.png",
     userInterfaceStyle: "automatic",
