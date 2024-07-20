4be647743671b56ae7bf9cd796ddbbedcf268c05
sezerad <sezeradil@gmail.com>
2024-07-20 01:01:53 +0100

    Custom text field ui improvement

--- src/components/CustomTextInput.tsx
+++ src/components/CustomTextInput.tsx
@@ -8,6 +8,7 @@ import {
   Platform,
   View,
   ViewStyle,
+  Animated,
 } from "react-native";
 import { useThemeStyles } from "@src/hooks/useThemeStyles";
 
@@ -33,6 +34,7 @@ const CustomTextInput: React.FC<CustomTextInputProps> = ({
   const { width: screenWidth } = useWindowDimensions();
 
   const [isFocused, setIsFocused] = useState(false);
+  const [placeholderAnim] = useState(new Animated.Value(0));
 
   // Determine if the device is a tablet
   const isTablet =
@@ -41,25 +43,72 @@ const CustomTextInput: React.FC<CustomTextInputProps> = ({
   // Determine the default width for iPad
   const defaultWidth = isTablet ? "75%" : "100%";
 
+  const handleFocus = () => {
+    setIsFocused(true);
+    Animated.timing(placeholderAnim, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+    Animated.timing(placeholderAnim, {
+      toValue: 0,
+      duration: 200,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const placeholderTranslateY = placeholderAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, -20], // Adjust the output range to control the vertical movement of the placeholder
+  });
+
+  const placeholderOpacity = placeholderAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0], // Fade out the placeholder when focused
+  });
+
   return (
     <View
       style={[
         {
           width: defaultWidth,
           alignSelf: "center",
+          position: "relative",
+          justifyContent: "center",
+          alignItems: "center",
         },
         containerStyle,
       ]}
     >
+      <Animated.Text
+        style={[
+          globalStyles.bold,
+          {
+            position: "absolute",
+            transform: [{ translateY: placeholderTranslateY }],
+            opacity: placeholderOpacity,
+            color: globalColors.placeholder,
+            textAlign: "center",
+          },
+        ]}
+      >
+        {placeholder}
+      </Animated.Text>
       <TextInput
-        style={[globalStyles.input, style, { width: "100%" }]}
-        placeholder={isFocused ? "" : placeholder}
+        style={[
+          globalStyles.input,
+          style,
+          { width: "100%", textAlign: "center" },
+        ]}
         value={value}
         onChangeText={onChangeText}
         secureTextEntry={secureTextEntry}
-        placeholderTextColor={globalColors.placeholder}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
         autoCapitalize="none"
         autoCorrect={false}
         {...rest}
