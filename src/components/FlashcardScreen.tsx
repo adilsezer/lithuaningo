34f906cdd9e8b102399c10cd4a93cdc70a29242f
sezerad <sezeradil@gmail.com>
2024-07-21 01:23:37 +0100

    Flashcard improvements

--- src/components/FlashcardScreen.tsx
+++ src/components/FlashcardScreen.tsx
@@ -192,121 +192,283 @@ const FlashcardScreen: React.FC<FlashcardScreenProps> = ({ wordId }) => {
     router.back();
   };
 
-  const explainGrammaticalForms = (form: string, isVerb: boolean): string => {
-    const verbEndingsMap = [
-      // Present Tense
-      { ending: "u", translation: "(I present)" },
-      { ending: "i", translation: "(you sing. present)" },
-      { ending: "a", translation: "(he/she/it present)" },
-      { ending: "ame", translation: "(we present)" },
-      { ending: "ate", translation: "(you pl. present)" },
-      { ending: "a", translation: "(they present)" },
-      // Past Tense
-      { ending: "au", translation: "(I past)" },
-      { ending: "ai", translation: "(you sing. past)" },
-      { ending: "o", translation: "(he/she/it past)" },
-      { ending: "ome", translation: "(we past)" },
-      { ending: "ote", translation: "(you pl. past)" },
-      { ending: "o", translation: "(they past)" },
-      // Future Tense
-      { ending: "siu", translation: "(I future)" },
-      { ending: "si", translation: "(you sing. future)" },
-      { ending: "s", translation: "(he/she/it future)" },
-      { ending: "sime", translation: "(we future)" },
-      { ending: "site", translation: "(you pl. future)" },
-      { ending: "s", translation: "(they future)" },
-      // Conditional
-      { ending: "čiau", translation: "(I would)" },
-      { ending: "tum", translation: "(you sing. would)" },
-      { ending: "tų", translation: "(he/she/it would)" },
-      { ending: "tume", translation: "(we would)" },
-      { ending: "tumėte", translation: "(you pl. would)" },
-      { ending: "tų", translation: "(they would)" },
-      // Participles
-      { ending: "damas", translation: "(he -ing)" },
-      { ending: "dama", translation: "(she -ing)" },
-      { ending: "damieji", translation: "(they masc. -ing)" },
-      { ending: "damosios", translation: "(they fem. -ing)" },
-      { ending: "ęs", translation: "(having masc.)" },
-      { ending: "usi", translation: "(having fem.)" },
-      { ending: "ęsi", translation: "(having fem. pl.)" },
-      // Imperative
-      { ending: "k", translation: "(you sing. imperative)" },
-      { ending: "kite", translation: "(you pl. imperative)" },
-    ];
+  const verbEndingsMap = [
+    { ending: "u", description: "present 1st sing." },
+    {
+      ending: "i",
+      description: "present 2nd sing.",
+    },
+    {
+      ending: "a",
+      description: "present 3rd sing.",
+    },
+    {
+      ending: "ame",
+      description: "present 1st pl.",
+    },
+    {
+      ending: "ate",
+      description: "present 2nd pl.",
+    },
+    {
+      ending: "a",
+      description: "present 3rd pl.",
+    },
+    { ending: "au", description: "past 1st sing." },
+    {
+      ending: "ai",
+      description: "past 2nd sing.",
+    },
+    {
+      ending: "o",
+      description: "past 3rd sing.",
+    },
+    { ending: "ome", description: "past 1st pl." },
+    {
+      ending: "ote",
+      description: "past 2nd pl.",
+    },
+    { ending: "o", description: "past 3rd pl." },
+    {
+      ending: "siu",
+      description: "future 1st sing.",
+    },
+    {
+      ending: "si",
+      description: "future 2nd sing.",
+    },
+    {
+      ending: "s",
+      description: "future 3rd sing.",
+    },
+    {
+      ending: "sime",
+      description: "future 1st pl.",
+    },
+    {
+      ending: "site",
+      description: "future 2nd pl.",
+    },
+    {
+      ending: "s",
+      description: "future 3rd pl.",
+    },
+    {
+      ending: "čiau",
+      description: "cond. 1st sing.",
+    },
+    {
+      ending: "tum",
+      description: "cond. 2nd sing.",
+    },
+    {
+      ending: "tų",
+      description: "cond. 3rd sing.",
+    },
+    {
+      ending: "tume",
+      description: "cond. 1st pl.",
+    },
+    {
+      ending: "tumėte",
+      description: "cond. 2nd pl.",
+    },
+    {
+      ending: "tų",
+      description: "cond. 3rd pl.",
+    },
+    {
+      ending: "damas",
+      description: "pres. part. masc.",
+    },
+    {
+      ending: "dama",
+      description: "pres. part. fem.",
+    },
+    {
+      ending: "damieji",
+      description: "pres. part. masc. pl.",
+    },
+    {
+      ending: "damosios",
+      description: "pres. part. fem. pl.",
+    },
+    {
+      ending: "ęs",
+      description: "past part. masc.",
+    },
+    {
+      ending: "usi",
+      description: "past part. fem.",
+    },
+    {
+      ending: "ęsi",
+      description: "past part. fem. pl.",
+    },
+    {
+      ending: "k",
+      description: "imper. 2nd sing.",
+    },
+    {
+      ending: "kite",
+      description: "imper. 2nd pl.",
+    },
+  ];
 
-    const nounEndingsMap = [
-      // Singular Noun Endings
-      { ending: "as", translation: "(masc. nom. sing.)" },
-      { ending: "is", translation: "(masc. nom. sing.)" },
-      { ending: "ys", translation: "(masc. nom. sing.)" },
-      { ending: "a", translation: "(fem. nom. sing.)" },
-      { ending: "ė", translation: "(fem. nom. sing.)" },
-      { ending: "ą", translation: "(acc. sing.)" },
-      { ending: "ę", translation: "(acc. sing.)" },
-      { ending: "o", translation: "(gen. sing. - of)" },
-      { ending: "ės", translation: "(gen. sing. - of)" },
-      { ending: "ui", translation: "(dat. sing. - to)" },
-      { ending: "iai", translation: "(dat. sing. - to)" },
-      { ending: "į", translation: "(acc. sing. - to)" },
-      { ending: "uje", translation: "(loc. sing. - in/at)" },
-      { ending: "ėje", translation: "(loc. sing. - in/at)" },
-      { ending: "yje", translation: "(loc. sing. - in/at)" }, // Added for "salis"
-      { ending: "imi", translation: "(inst. sing. - with/by)" },
-      { ending: "umi", translation: "(inst. sing. - with/by)" },
-      // Plural Noun Endings
-      { ending: "ai", translation: "(masc. nom. pl.)" },
-      { ending: "ys", translation: "(masc. nom. pl.)" },
-      { ending: "os", translation: "(fem. nom. pl./gen. pl. - of)" },
-      { ending: "ios", translation: "(fem. nom. pl.)" },
-      { ending: "es", translation: "(fem. nom. pl.)" },
-      { ending: "us", translation: "(acc. pl. masc.)" },
-      { ending: "as", translation: "(acc. pl. fem.)" },
-      { ending: "ių", translation: "(gen. pl. - of)" },
-      { ending: "ų", translation: "(gen. pl. - of)" },
-      { ending: "omis", translation: "(inst. pl. - with/by)" },
-      { ending: "imis", translation: "(inst. pl. - with/by)" },
-      { ending: "iais", translation: "(inst. pl. - with/by)" },
-      { ending: "uose", translation: "(loc. pl. - in/at)" },
-      { ending: "iuose", translation: "(loc. pl. - in/at)" },
-      { ending: "ėse", translation: "(loc. pl. - in/at)" },
-      // Adjective Endings
-      { ending: "us", translation: "(masc. adj. nom. sing.)" },
-      { ending: "i", translation: "(fem. adj. nom. sing.)" },
-      { ending: "ius", translation: "(masc. adj. nom. pl.)" },
-      { ending: "ios", translation: "(fem. adj. nom. pl.)" },
-      { ending: "ų", translation: "(gen. adj. sing. masc. - of)" },
-      { ending: "ių", translation: "(gen. adj. pl. - of)" },
-      { ending: "am", translation: "(dat. adj. sing. masc. - to)" },
-      { ending: "iam", translation: "(dat. adj. sing. fem. - to)" },
-      { ending: "ąją", translation: "(acc. adj. sing. fem.)" },
-      { ending: "uoju", translation: "(inst. adj. sing. masc. - with/by)" },
-      { ending: "ąja", translation: "(inst. adj. sing. fem. - with/by)" },
-      { ending: "uosius", translation: "(acc. adj. pl. masc.)" },
-      { ending: "osioms", translation: "(dat. adj. pl. fem. - to)" },
-      // Locative Endings
-      { ending: "oje", translation: "(loc. sing. - in/at)" },
-      { ending: "ėje", translation: "(loc. sing. - in/at)" },
-      { ending: "yje", translation: "(loc. sing. - in/at)" }, // Added for "salis"
-      { ending: "uose", translation: "(loc. pl. - in/at)" },
-      { ending: "iuose", translation: "(loc. pl. - in/at)" },
-    ];
+  const nounEndingsMap = [
+    { ending: "as", description: "masc. nom. sing." },
+    { ending: "is", description: "masc. nom. sing." },
+    { ending: "ys", description: "masc. nom. sing." },
+    { ending: "a", description: "fem. nom. sing." },
+    { ending: "ė", description: "fem. nom. sing." },
+    { ending: "ą", description: "acc. sing." },
+    { ending: "ę", description: "acc. sing." },
+    { ending: "o", description: "gen. sing." },
+    { ending: "ės", description: "gen. sing." },
+    { ending: "ui", description: "dat. sing." },
+    { ending: "iai", description: "dat. sing." },
+    { ending: "į", description: "acc. sing." },
+    { ending: "uje", description: "loc. sing." },
+    { ending: "ėje", description: "loc. sing." },
+    { ending: "yje", description: "loc. sing." },
+    {
+      ending: "imi",
+      description: "inst. sing.",
+    },
+    {
+      ending: "umi",
+      description: "inst. sing.",
+    },
+    { ending: "ai", description: "masc. nom. pl." },
+    { ending: "ys", description: "masc. nom. pl." },
+    { ending: "os", description: "fem. nom. pl." },
+    { ending: "ios", description: "fem. nom. pl." },
+    { ending: "es", description: "fem. nom. pl." },
+    { ending: "us", description: "acc. pl. masc." },
+    { ending: "as", description: "acc. pl. fem." },
+    { ending: "ių", description: "gen. pl." },
+    { ending: "ų", description: "gen. pl." },
+    {
+      ending: "omis",
+      description: "inst. pl.",
+    },
+    {
+      ending: "imis",
+      description: "inst. pl.",
+    },
+    {
+      ending: "iais",
+      description: "inst. pl.",
+    },
+    { ending: "uose", description: "loc. pl." },
+    { ending: "iuose", description: "loc. pl." },
+    { ending: "ėse", description: "loc. pl." },
+    { ending: "us", description: "masc. acc. sing." },
+    { ending: "i", description: "fem. acc. sing." },
+    { ending: "ius", description: "masc. nom. pl." },
+    { ending: "ios", description: "fem. nom. pl." },
+    { ending: "ų", description: "gen. pl." },
+    { ending: "ių", description: "gen. pl." },
+    {
+      ending: "am",
+      description: "dat. sing. masc.",
+    },
+    {
+      ending: "iam",
+      description: "dat. sing. fem.",
+    },
+    {
+      ending: "ąją",
+      description: "acc. sing. fem.",
+    },
+    {
+      ending: "uoju",
+      description: "inst. sing. masc.",
+    },
+    {
+      ending: "ąja",
+      description: "inst. sing. fem.",
+    },
+    {
+      ending: "uosius",
+      description: "acc. pl. masc.",
+    },
+    {
+      ending: "osioms",
+      description: "dat. pl. fem.",
+    },
+    { ending: "oje", description: "loc. sing." },
+    { ending: "ėje", description: "loc. sing." },
+    { ending: "yje", description: "loc. sing." },
+    { ending: "uose", description: "loc. pl." },
+    { ending: "iuose", description: "loc. pl." },
+    {
+      ending: "us",
+      description: "masc. adj. nom. sing.",
+    },
+    {
+      ending: "i",
+      description: "fem. adj. nom. sing.",
+    },
+    {
+      ending: "ius",
+      description: "masc. adj. nom. pl.",
+    },
+    {
+      ending: "ios",
+      description: "fem. adj. nom. pl.",
+    },
+    {
+      ending: "ų",
+      description: "gen. adj. sing. masc.",
+    },
+    {
+      ending: "ių",
+      description: "gen. adj. pl.",
+    },
+    {
+      ending: "am",
+      description: "dat. adj. sing. masc.",
+    },
+    {
+      ending: "iam",
+      description: "dat. adj. sing. fem.",
+    },
+    {
+      ending: "ąją",
+      description: "acc. adj. sing. fem.",
+    },
+    {
+      ending: "uoju",
+      description: "inst. adj. sing. masc.",
+    },
+    {
+      ending: "ąja",
+      description: "inst. adj. sing. fem.",
+    },
+    {
+      ending: "uosius",
+      description: "acc. adj. pl. masc.",
+    },
+    {
+      ending: "osioms",
+      description: "dat. adj. pl. fem.",
+    },
+  ];
 
-    const endingsMap = isVerb ? verbEndingsMap : nounEndingsMap;
+  const explainGrammaticalForms = (
+    form: string,
+    baseTranslation: string
+  ): { description: string } => {
+    const isVerb = baseTranslation.toLowerCase().startsWith("to ");
+    const endingsMap = (isVerb ? verbEndingsMap : nounEndingsMap)
+      .slice()
+      .sort((a, b) => b.ending.length - a.ending.length);
 
-    // Sort endings by length in descending order to ensure specific matches first
-    endingsMap.sort((a, b) => b.ending.length - a.ending.length);
+    const endingEntry = endingsMap.find(({ ending }) => form.endsWith(ending));
+    const description = endingEntry ? endingEntry.description : "";
 
-    for (const { ending, translation } of endingsMap) {
-      if (form.endsWith(ending)) {
-        return `${form} ${translation}`;
-      }
-    }
-    return `${form}`;
-  };
-
-  const determineIfVerb = (englishTranslation: string): boolean => {
-    return englishTranslation.toLowerCase().startsWith("to ");
+    return { description };
   };
 
   return (
@@ -351,10 +513,12 @@ const FlashcardScreen: React.FC<FlashcardScreenProps> = ({ wordId }) => {
                               { marginLeft: 8, flexShrink: 1 },
                             ]}
                           >
-                            {explainGrammaticalForms(
-                              form,
-                              determineIfVerb(word.englishTranslation)
-                            )}
+                            {`${form} - ${
+                              explainGrammaticalForms(
+                                form,
+                                word.englishTranslation
+                              ).description
+                            }`}
                           </Text>
                         </View>
                       )
