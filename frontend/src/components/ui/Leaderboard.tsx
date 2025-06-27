1f23ef674f170a704c9166d371cbc6d3b82c494a
sezerad <sezeradil@gmail.com>
2025-06-28 02:10:34 +0300

    Refactor Leaderboard component for improved styling and structure
    - Consolidated DataTable header structure for better readability and organization.
    - Enhanced row styling with border properties for a more polished appearance.
    - Maintained existing functionality while improving the overall layout and user experience.
    
    These changes aim to enhance the visual consistency and usability of the Leaderboard component.

--- frontend/src/components/ui/Leaderboard.tsx
+++ frontend/src/components/ui/Leaderboard.tsx
@@ -29,66 +29,71 @@ const Leaderboard = ({ entries }: LeaderboardProps) => {
   const theme = useTheme();
 
   return (
-    <View>
-      <DataTable
-        style={[styles.tableContainer, { borderColor: theme.colors.primary }]}
+    <DataTable
+      style={[styles.tableContainer, { borderColor: theme.colors.primary }]}
+    >
+      {/* Title integrated into header */}
+      <DataTable.Header
+        style={[styles.header, { backgroundColor: theme.colors.primary }]}
       >
-        {/* Title integrated into header */}
-        <DataTable.Header
-          style={[styles.header, { backgroundColor: theme.colors.primary }]}
-        >
-          <DataTable.Title style={styles.titleCell}>
-            <CustomText variant="titleSmall" bold color="white">
-              Top 20 Leaders This Week
-            </CustomText>
-          </DataTable.Title>
-        </DataTable.Header>
+        <DataTable.Title style={styles.titleCell}>
+          <CustomText variant="titleSmall" bold color="white">
+            Top 20 Leaders This Week
+          </CustomText>
+        </DataTable.Title>
+      </DataTable.Header>
 
-        {/* Table Column Headers */}
-        <DataTable.Header
-          style={{
-            backgroundColor: theme.colors.surface,
-          }}
-        >
-          <DataTable.Title>Rank</DataTable.Title>
-          <DataTable.Title style={{ flex: 3 }}>Name</DataTable.Title>
-          <DataTable.Title numeric>Score</DataTable.Title>
-        </DataTable.Header>
+      {/* Table Column Headers */}
+      <DataTable.Header
+        style={{
+          backgroundColor: theme.colors.surface,
+        }}
+      >
+        <DataTable.Title>Rank</DataTable.Title>
+        <DataTable.Title style={{ flex: 3 }}>Name</DataTable.Title>
+        <DataTable.Title numeric>Score</DataTable.Title>
+      </DataTable.Header>
 
-        {/* Data Rows */}
-        {entries.length > 0 ? (
-          entries.map((entry, index) => (
-            <DataTable.Row key={entry.userId}>
-              <DataTable.Cell>{index + 1}</DataTable.Cell>
-              <DataTable.Cell style={{ flex: 3 }}>
-                <View style={styles.nameView}>
-                  <TrophyIcon position={index} color={theme.colors.primary} />
-                  <CustomText>{entry.username}</CustomText>
-                </View>
-              </DataTable.Cell>
-              <DataTable.Cell numeric>{entry.score}</DataTable.Cell>
-            </DataTable.Row>
-          ))
-        ) : (
-          <DataTable.Row>
-            <DataTable.Cell style={styles.emptyCell}>
-              <CustomText>
-                Be the first to make it to the leaderboard!{"\n"}
-                We're currently waiting for new leaders to emerge.
-              </CustomText>
+      {/* Data Rows */}
+      {entries.length > 0 ? (
+        entries.map((entry, index) => (
+          <DataTable.Row
+            key={entry.userId}
+            style={{
+              borderBottomColor: theme.colors.primary,
+              borderBottomLeftRadius: 12,
+              borderBottomRightRadius: 12,
+            }}
+          >
+            <DataTable.Cell>{index + 1}</DataTable.Cell>
+            <DataTable.Cell style={{ flex: 3 }}>
+              <View style={styles.nameView}>
+                <TrophyIcon position={index} color={theme.colors.primary} />
+                <CustomText>{entry.username}</CustomText>
+              </View>
             </DataTable.Cell>
+            <DataTable.Cell numeric>{entry.score}</DataTable.Cell>
           </DataTable.Row>
-        )}
-      </DataTable>
-    </View>
+        ))
+      ) : (
+        <DataTable.Row>
+          <DataTable.Cell style={styles.emptyCell}>
+            <CustomText>
+              Be the first to make it to the leaderboard!{"\n"}
+              We're currently waiting for new leaders to emerge.
+            </CustomText>
+          </DataTable.Cell>
+        </DataTable.Row>
+      )}
+    </DataTable>
   );
 };
 
 const styles = StyleSheet.create({
   tableContainer: {
-    borderWidth: 1,
-    borderRadius: 12,
     marginVertical: 10,
+    borderWidth: 0.5,
+    borderRadius: 12,
   },
   header: {
     borderTopLeftRadius: 10,
